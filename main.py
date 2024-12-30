f2d0fb16676e57a9974bafc1ed0e267cd65e57d6
Gabriel Chaves <teixeira.gabriel11@gmail.com>
2024-12-29 21:28:17 -0300

    construção dos demais modulos

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def homepage():
-    return render_template("homepage.html")
-
-@app.route("/perfil")
-def perfil():
-    return render_template("perfil.html")
+from fakepinterest import app
 
 if __name__ == "__main__":
     app.run(debug=True)
